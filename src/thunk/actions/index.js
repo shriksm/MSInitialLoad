3c04fa3aca0b101b946e5a229093d86c7c4aa423
smanivannan <smanivannan@logitech.com>
2017-11-10 17:10:23 -0800

    removed unused folders and files

--- src/thunk/actions/index.js
+++
@@ -1,9 +0,0 @@
-import * as navigationActions from './navigation'
-import * as dataActions from './login'
-
-export const ActionCreators = Object.assign({},
-  navigationActions,
-  dataActions
-);
-
-console.log('ActionCreators: ' + JSON.stringify(ActionCreators));
